c32069a169087477b54b64f1a2a85a20864a5cb2
liuzhen19 <liuzhen19@xiaomi.com>
2026-02-19 10:39:33 +0800

    upd
    Signed-off-by: liuzhen19 <liuzhen19@xiaomi.com>

--- scripts/install_fonts.sh
+++ scripts/install_fonts.sh
@@ -22,25 +22,35 @@ download_if_missing() {
 # Required by template.typ:
 # - "Noto Sans CJK SC"
 # - "FiraCode Nerd Font Mono"
-download_if_missing \
-  "${FONT_DIR}/NotoSansCJKsc-Regular.otf" \
-  "https://raw.githubusercontent.com/notofonts/noto-cjk/main/Sans/OTF/SimplifiedChinese/NotoSansCJKsc-Regular.otf"
+# download_if_missing \
+#   "${FONT_DIR}/NotoSansCJKsc-Regular.otf" \
+#   "https://raw.githubusercontent.com/notofonts/noto-cjk/main/Sans/OTF/SimplifiedChinese/NotoSansCJKsc-Regular.otf"
 
-download_if_missing \
-  "${FONT_DIR}/FiraCodeNerdFontMono-Regular.ttf" \
-  "https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/FiraCode/Regular/FiraCodeNerdFontMono-Regular.ttf"
+# download_if_missing \
+#   "${FONT_DIR}/FiraCodeNerdFontMono-Regular.ttf" \
+#   "https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/FiraCode/Regular/FiraCodeNerdFontMono-Regular.ttf"
 
-# https://github.com/FortAwesome/Font-Awesome/blob/7.x/webfonts/fa-brands-400.woff2
-download_if_missing \
-  "${FONT_DIR}/fa-brands-400.woff2" \
-  "https://raw.githubusercontent.com/FortAwesome/Font-Awesome/7.x/webfonts/fa-brands-400.woff2"
+# # https://github.com/FortAwesome/Font-Awesome/blob/7.x/webfonts/fa-brands-400.woff2
+# download_if_missing \
+#   "${FONT_DIR}/fa-brands-400.woff2" \
+#   "https://raw.githubusercontent.com/FortAwesome/Font-Awesome/7.x/webfonts/fa-brands-400.woff2"
 
-download_if_missing \
-  "${FONT_DIR}/fa-regular-400.woff2" \
-  "https://raw.githubusercontent.com/FortAwesome/Font-Awesome/7.x/webfonts/fa-regular-400.woff2"
+# download_if_missing \
+#   "${FONT_DIR}/fa-regular-400.woff2" \
+#   "https://raw.githubusercontent.com/FortAwesome/Font-Awesome/7.x/webfonts/fa-regular-400.woff2"
 
-download_if_missing \
-  "${FONT_DIR}/fa-solid-900.woff2" \
-  "https://raw.githubusercontent.com/FortAwesome/Font-Awesome/7.x/webfonts/fa-solid-900.woff2"
+# download_if_missing \
+#   "${FONT_DIR}/fa-solid-900.woff2" \
+#   "https://raw.githubusercontent.com/FortAwesome/Font-Awesome/7.x/webfonts/fa-solid-900.woff2"
 
-echo "[fonts] installed into ${FONT_DIR}"
+# fa fonts need to be install in ubuntu
+# node-fortawesome-fontawesome-free
+# node-fortawesome-fontawesome-svg-core
+
+apt-get update
+apt-get install -y --no-install-recommends \
+  fonts-noto-cjk \
+  fonts-firacode \
+  fonts-font-awesome
+
+# echo "[fonts] installed into ${FONT_DIR}"
